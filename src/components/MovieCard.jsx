a24c8f098fd0db3d83d7b908e9111ab423055015
Aaron Primmer <aaronmprimmer@gmail.com>
2025-10-17 17:17:40 -0400

    added pages files and movie card

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,24 @@
+function MovieCard({ movieInfo }) {
+  function onFacvoriteClick() {
+    alert("clicked");
+  }
+
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img src={movieInfo.url} alt={movieInfo.title} />
+      </div>
+      <div className="movie-overlay">
+        <button className="favorite-btn" onClick={onFacvoriteClick}>
+          ♥
+        </button>
+      </div>
+      <div className="movie-info">
+        <h3>{movieInfo.title}</h3>
+        <p>{movieInfo.release_date}</p>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
